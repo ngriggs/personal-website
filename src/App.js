26e55afceab3b7fc01c31382c897d284f790cb7a
Noah Griggs <noahg@vt.edu>
2021-06-22 18:20:28 -0500

    added some animations to the navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ProjectList from "./components/ProjectList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NavBar extends React.Component {
+  state = {
+    hamburgerClass: "hamburger",
+    isActive: false,
+  };
+  handleClick = () => {
+    this.setState({
+      hamburgerClass: this.state.isActive ? "hamburger" : "hamburger is-active",
+      isActive: !this.state.isActive,
+    });
+  };
+
+  render() {
+    return (
+      <nav className="nav-bar-items">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+                padding: "1em",
+              }}
+            >
+              <img
+                style={{ height: "22px" }}
+                src={logo}
+                className="App-logo"
+                alt="logo"
+              />
+              Noah
+            </div>
+          </div>
+          <div className="menu">
+            <div
+              style={{
+                display: "flex",
+                padding: "1em",
+              }}
+            >
+              About
+            </div>
+            <div
+              style={{
+                display: "flex",
+                padding: "1em",
+              }}
+            >
+              Technologies
+            </div>
+          </div>
+          <button
+            className={this.state.hamburgerClass}
+            onClick={this.handleClick}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </nav>
+    );
+  }
+}
+
+class App extends React.Component {
+  componentDidMount() {
+    document.title = "Noah Griggs";
+  }
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="App-header">
+          <ProjectList />
+        </div>
+        <ProjectList />
+        <ProjectList />
+        <ProjectList />
+        <ProjectList />
+        <ProjectList />
+      </div>
+    );
+  }
 }
 
 export default App;
